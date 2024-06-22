870a0cdb77dddd20b85282706a1b85628c173e46
saalimh <59394790+saalimh@users.noreply.github.com>
2024-06-22 13:46:05 +0100

    Create UserCard & UserPage component

---
+++ cest-la-vue/src/components/UserCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <li class="user-card">{{ user.name }}: {{ user.website }}</li>
+  </div>
+</template>
+
+<style>
+.user-card {
+  border: 2px solid black;
+  padding: 10px;
+}
+</style>
---
+++ cest-la-vue/src/components/UserPage.vue
@@ -0,0 +1,60 @@
+<script>
+import UserCard from "./UserCard.vue";
+export default {
+  components: {
+    UserCard,
+  },
+  data: () => ({
+    userList: [],
+  }),
+  methods: {
+    async fetchUsers() {
+      this.userList = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    },
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
+</script>
+<template>
+  <h1>Users</h1>
+  <ul>
+    <UserCard v-for="user in userList" :key="`user-${user.id}`" :user="user" />
+  </ul>
+  <!-- <pre>{{ userList }}</pre> -->
+</template>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 320px;
+  margin: 0 auto;
+}
+
+main h1 {
+  margin-top: 10vh;
+  margin-bottom: 20px;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input[type="email"] {
+  padding: 0.5rem;
+  margin-bottom: 30px;
+}
+
+button {
+  border: 1px solid green;
+  padding: 10px;
+  color: green;
+  background-color: rgb(213, 255, 213);
+  cursor: pointer;
+}
+</style>
