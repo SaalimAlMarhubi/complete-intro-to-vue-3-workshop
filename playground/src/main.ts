5a29515b5e9ccd8337ee06c4f21f36946811f9f6
saalimh <59394790+saalimh@users.noreply.github.com>
2024-09-28 08:37:02 +0100

    Formatting

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,16 +1,16 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { createPinia } from 'pinia'
-import { routes } from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import { createPinia } from "pinia";
+import { routes } from "./router";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 const router = createRouter({
-   history: createWebHashHistory(),
-   routes
-})
-const pinia = createPinia()
+  history: createWebHashHistory(),
+  routes,
+});
+const pinia = createPinia();
 
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+app.mount("#app");
